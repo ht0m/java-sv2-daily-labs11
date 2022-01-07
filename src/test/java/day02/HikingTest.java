4d3cc9431d5cc7efab7641522a3de014390b2831
ht0m <hartht@gmail.com>
2022-01-07 17:17:07 +0100

    Hiking

--- src/test/java/day02/HikingTest.java
+++ src/test/java/day02/HikingTest.java
@@ -13,8 +13,8 @@ class HikingTest {
     @Test
     void testHiking() {
 
-        List<Double> heights = new ArrayList<>(Arrays.asList(10.0,20.0,15.0,18.0));
-        assertEquals(13.0,new Hiking().getPlusElevation(heights));
+        List<Double> heights = new ArrayList<>(Arrays.asList(10.0,20.0,25.0,18.0));
+        assertEquals(15.0,new Hiking().getPlusElevation(heights));
 
         List<Double> heights2 = new ArrayList<>(Arrays.asList(0.0,20.0));
         assertEquals(20.0,new Hiking().getPlusElevation(heights2));
